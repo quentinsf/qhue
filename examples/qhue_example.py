162d2867d865d2d3bd2c77bae8b8917fc0a16d1c
Chris Macklin <chris.macklin@gmail.com>
2016-02-15 15:33:09 -0800

    Complete package restructuring.

--- examples/qhue_example.py
+++ examples/qhue_example.py
@@ -1,44 +1,49 @@
-# load dependency for path.exists()
+#!/usr/bin/env python
+
 from os import path
+from qhue import Bridge, QhueException
 
-# load dependency for Bridge()
-from qhue.qhue import Bridge
+# the IP address of your bridge
+BRIDGE_IP = "192.168.2.3"
 
-# load dependency for exc_info()
-from sys import exc_info
+# the path for the username credentials file
+CRED_FILE_PATH = "qhue_username.txt"
 
-# check for a credential file
-if not path.exists("qhue_username.txt"):
-  # create a bridge resource
-  b = Bridge("192.168.0.45")
+def main():
 
-  # try connecting anonymously to the bridge
-  try:
-    username = b(devicetype="testdev", http_method="post")[0]["success"]["username"]
-  # catch the error which is raised and print its message
-  except:
-    print(exc_info()[1])
+    # check for a credential file
+    if not path.exists(CRED_FILE_PATH):
+        # create a bridge resource
+        bridge = Bridge(BRIDGE_IP)
 
-  # press the bridge button
-  raw_input("Press Return after pressing the bridge button")
+        while True:
+            # press the bridge button
+            raw_input("Press the bridge button, then press Return.")
 
-  # connect anonymously to the bridge again, to create an user
-  username = b(devicetype="testdev", http_method="post")[0]["success"]["username"]
+            # connect anonymously to the bridge to create a user
+            try:
+                response = bridge(devicetype="testdev", http_method="post")
+                username = response[0]["success"]["username"]
+                break
+            except QhueException as err:
+                print "Error occurred while creating a new username: {}".format(err)
 
-  # store the username in a credential file
-  file = open("qhue_username.txt", "w")
-  file.write(username)
-  file.close()
-else:
-  file = open("qhue_username.txt", "r")
-  username = file.read()
-  file.close()
+        # store the username in a credential file
+        with open(CRED_FILE_PATH, "w") as cred_file:
+            cred_file.write(username)
 
-# (re)create the bridge resource, passing the captured username
-b = Bridge("192.168.0.45", username)
+    else:
+        with open(CRED_FILE_PATH, "r") as cred_file:
+            username = cred_file.read()
 
-# create a lights resource
-lights = b.lights
+    # (re)create the bridge resource, passing the captured username
+    bridge = Bridge(BRIDGE_IP, username)
 
-# query the API and print the results
-print lights()
+    # create a lights resource
+    lights = bridge.lights
+
+    # query the API and print the results
+    print lights()
+
+if __name__ == "__main__":
+    main()
