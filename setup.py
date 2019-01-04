a70c6231eb41fd5f8e82e73f54efd35cfc76b533
Quentin Stafford-Fraser <quentin@pobox.com>
2019-01-04 08:53:40 +0000

    Include the README as the long_description for the package.

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ build_version = 11
 
 version = str(major_version) + '.' + str(minor_version) + '.' + str(build_version)
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='qhue',
     version=version,
     description='Qhue: python wrapper for Philips Hue API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Quentin Stafford-Fraser',
     url='https://github.com/quentinsf/qhue',
     license='GNU GPL 2',
